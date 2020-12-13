4d4ab597d595c0ad57dd8dee0d6432b179b421c0
Cesar Velasco Arias <cesar091611pm@gmail.com>
2020-12-13 23:33:53 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var DataStore = require('nedb');
+
+var port = (process.env.PORT || 3030);
+var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname + "/contacts.json";
+
+console.log("Starting API Server...");
+
+
+var app = express();
+app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
+
+
+app.get("/", (req, res) => {
+    res.send("<html><body><h1>CAVA 1611 !!</h1></body></html>");
+});
+
+app.get(BASE_API_PATH + "/contacts", (req, res) =>{
+    console.log(Date() + "- GET /contacts");
+    db.find({}, (err, contacts) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        } else {
+            res.send(contacts.map((contact) => {
+                delete contact._id;
+                return contact;
+            }));
+        }
+    });
+});
+
+app.post(BASE_API_PATH + "/contacts", (req, res) => {
+    console.log(Date() + "- POST /contacts");
+    var contact = req.body;
+    db.insert(contact, (err) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res.send(500);
+        } else {
+            res.sendStatus(201);
+        }
+    });
+});
+
+app.listen(port);
+
+console.log("Server ready...!!");
