5b779485decbfc045f4e4a226163f140048556e1
Cesar Velasco <cesar091611pm@gmail.com>
2021-01-07 13:49:59 +0000

    update files  index & package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('../prueba_2/server');
-const dbConnect = require('../prueba_2/db');
+const app = require('./server');
+const dbConnect = require('./db');
 
 var port = (process.env.PORT || 3030);
 
